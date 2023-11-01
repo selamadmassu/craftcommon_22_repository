497c7393632a2234fa4d0c0d64a81a39d74ae39c
craftgithubuser <craftknowledge.soresa@gmail.com>
2023-11-01 05:14:03 +0300

    I added java code to the repository

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+package com.icraft.day1;
+
+
+public class HelloWorld {
+	
+	//regular methods
+	
+	
+	//main method
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Team,...");
+		
+	}
+
+}
+
+
